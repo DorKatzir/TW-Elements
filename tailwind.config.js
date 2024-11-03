ca5924d966aec4ed46e596fb35bb649871d6a279
Dor Katzir <katzirdor@gmail.com>
2024-11-03 12:56:02 +0200

    Configure template paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
-  content: [],
+
+  content: ["./src/**/*.{html,js}"],
+
   theme: {
     extend: {},
   },
+
   plugins: [
   
   ],
+
 }
 
