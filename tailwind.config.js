917e00c9021875bcd0dac7c7d7c28ff36454ddc4
Dor Katzir <katzirdor@gmail.com>
2024-11-03 12:11:14 +0200

    change Tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{html,js}",
+    "./node_modules/tw-elements/js/**/*.js",
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+  plugins: [require("tw-elements/plugin.cjs")],
+  darkMode: "class"
+};
